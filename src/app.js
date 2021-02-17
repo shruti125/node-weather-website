3a6a07aaebbb4d3539664812364047fe2fae1e68
Shruti Sapre <shrutisapre125@gmail.com>
2021-02-17 08:21:01 -0500

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirPath = (path.join(__dirname, '../public'));
@@ -79,6 +80,6 @@ app.get('*', (req, res) => {
     res.render('notfound', {title: "404", message:"Page not found", name: 'Shruti S.'});
 })
 
-app.listen(3000, ()=> {
-    console.log('Server is running');  
+app.listen(port, ()=> {
+    console.log('Server is running on port' + port);  
 })
